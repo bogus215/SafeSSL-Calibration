e844a9b8d4f7e74a5fe8563a54ced3febc3ce1ad
bogus215 <wlstn215@gmail.com>
2023-09-06 13:16:45 +0900

    Update

--- scripts/SL.sh
+++ scripts/SL.sh
@@ -1,27 +1,23 @@
-N_LABELS=(400 100)
-N_VALID=(500 50)
+CIFAR10_SEEDS=(2 6 7 8 9)
+CIFAR100_SEEDS=(1 2 5 6 10)
 
-for seed in 1;
+for seed in 0 1 2 3 4
 do
-    A=0
-    for data in cifar10 cifar100;
-    do
-        echo $seed, $data, ${N_LABELS[$A]}, ${N_VALID[$A]}
-        python ./main/run_SL.py \
-        --gpus 0 \
-        --seed $seed \
-        --data $data \
-        --server main \
-        --enable-wandb \
-        --n-label-per-class ${N_LABELS[$A]} \
-        --n-valid-per-class ${N_VALID[$A]} \
-        --mismatch-ratio 0.60 \
-        --mixed-precision \
-        --save-every 5000 \
-        --learning-rate 3e-3 \
-        --backbone-type wide28_2 \
-        --optimizer adam \
-        --weight-decay 0
-        let "A+=1"
-    done
+    python ./main/run_SL.py --gpus 0 --seed ${CIFAR10_SEEDS[$seed]} \
+                            --data cifar10 --server main --enable-wandb \
+                            --n-label-per-class 400 \
+                            --n-valid-per-class 500 \
+                            --mismatch-ratio 0.60 --mixed-precision \
+                            --save-every 5000 --learning-rate 3e-3 \
+                            --backbone-type wide28_2 --optimizer adam \
+                            --weight-decay 0
+
+    python ./main/run_SL.py --gpus 0 --seed ${CIFAR100_SEEDS[$seed]} \
+                            --data cifar100 --server main --enable-wandb \
+                            --n-label-per-class 100 \
+                            --n-valid-per-class 50 \
+                            --mismatch-ratio 0.60 --mixed-precision \
+                            --save-every 5000 --learning-rate 3e-3 \
+                            --backbone-type wide28_2 --optimizer adam \
+                            --weight-decay 0
 done
