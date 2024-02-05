fe9e1baaabd7adae33ada4f969ac6147945ac438
bogus215 <wlstn215@gmail.com>
2024-02-06 08:44:45 +0900

    testing.py update

--- tasks/testing.py
+++ tasks/testing.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 from rich.progress import Progress
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
 from tasks.base import Task
 from utils.metrics import TopKAccuracy
@@ -86,7 +87,7 @@ class Testing(Task):
             eval_history = self.iomatch_evaluate(open_test_loader)
         elif for_what=="OPENMATCH":
             eval_history = self.openmatch_evaluate(open_test_loader)
-        elif for_what=="FIXMATCH":
+        elif for_what in ["FIXMATCH","SL"]:
             eval_history = self.fixmatch_evaluate(open_test_loader)
         else:
             pass
@@ -211,10 +212,11 @@ class Testing(Task):
         result = {
             'top@1': torch.zeros(1, device=self.local_rank),
             'ECE': np.zeros(1),
-            "F1": np.zeros(1)
+            "F1": np.zeros(1),
+            "SEEN-DETECTION-ECE": np.zeros(1),
         }
 
-        labels, logits, out_scores = [], [], []
+        labels, logits, out_scores, in_scores = [], [], [], []
 
         with Progress(transient=True, auto_refresh=False) as pg:
 
@@ -234,17 +236,19 @@ class Testing(Task):
 
                 pred_close = logit.data.max(1)[1]
                 unk_score = logits_open[tmp_range, 0, pred_close]
+                sen_score = logits_open[tmp_range, 1, pred_close]
 
                 labels.append(y.cpu())
                 logits.append(logit.cpu())
                 out_scores.append(unk_score.cpu())
+                in_scores.append(sen_score.cpu())
                 
                 if self.local_rank == 0:
                     desc = f"[bold pink] [{i+1}/{steps}] |"
                     pg.update(task, advance=1., description=desc)
                     pg.refresh()
 
-        labels, logits, out_scores = torch.cat(labels,axis=0), torch.cat(logits,axis=0), torch.cat(out_scores,axis=0)
+        labels, logits, out_scores, in_scores = torch.cat(labels,axis=0), torch.cat(logits,axis=0), torch.cat(out_scores,axis=0), torch.cat(in_scores,axis=0)
         
         in_pred = (out_scores < 0.5).cpu()
         in_label = torch.where(labels<logits.size(1),1,0)
@@ -252,6 +256,7 @@ class Testing(Task):
         result['top@1'][0] = TopKAccuracy(k=1)(logits[labels<logits.size(1)], labels[labels<logits.size(1)])
         result['ECE'][0] = self.get_ece(preds=logits[labels<logits.size(1)].softmax(dim=1).numpy(), targets = labels[labels<logits.size(1)].numpy())
         result['F1'][0] = f1_score(y_true=in_label, y_pred=in_pred)
+        result['SEEN-DETECTION-ECE'][0] = self.seen_unseen_detection_get_ece(predicted_label=in_pred.numpy(),confidences=in_scores.numpy(),targets=in_label.numpy(),n_bins=15)
 
         return {k: v.mean().item() for k, v in result.items()}
         
@@ -264,7 +269,9 @@ class Testing(Task):
         result = {
             'top@1': torch.zeros(1, device=self.local_rank),
             'ECE': np.zeros(1),
-            "F1": np.zeros(1)
+            "F1": np.zeros(1),
+            'In distribution over conf 0.95: ECE': np.zeros(1),
+            'In distribution under ood score 0.5: ECE': np.zeros(1),
         }
 
         labels, logits, domain_logits = [], [], []
@@ -302,7 +309,9 @@ class Testing(Task):
         
         from sklearn.metrics import f1_score
         result['F1'][0] = f1_score(y_true=in_label, y_pred=in_pred)
-
+        result['In distribution over conf 0.95: ECE'][0] = self.get_ece(preds=logits[(labels<logits.size(1)) & (logits.softmax(1).max(1)[0]>=0.95)].softmax(dim=1).numpy(), targets = labels[(labels<logits.size(1)) & (logits.softmax(1).max(1)[0]>=0.95)].numpy(), plot_title="_conf_over_95")
+        result['In distribution under ood score 0.5: ECE'][0] = self.get_ece(preds=logits[(labels<logits.size(1)) & (in_pred)].softmax(dim=1).numpy(), targets = labels[(labels<logits.size(1)) & (in_pred)].numpy(), plot_title="_ood_score_under_05")
+        
         return {k: v.mean().item() for k, v in result.items()}
 
     def predict(self, x: torch.FloatTensor):
@@ -315,7 +324,7 @@ class Testing(Task):
         else:
             self.backbone.eval()
 
-    def get_ece(self, preds: np.array, targets: np.array, n_bins: int=15):
+    def get_ece(self, preds: np.array, targets: np.array, n_bins: int=15, **kwargs):
 
         bin_boundaries = np.linspace(0,1,n_bins+1)
         bin_lowers = bin_boundaries[:-1]
@@ -350,4 +359,92 @@ class Testing(Task):
                 x_ticks.append(None)
                 y_ticks_second_ticks.append(None)
 
+        plot_title = kwargs.get("plot_title", "_all")
+
+        plt.bar(np.array(x_ticks)[np.array(x_ticks)!=None], np.array(confs_ticks)[np.array(x_ticks)!=None], label = 'Confidence', alpha = .7, ecolor = 'blue', width = bin_lowers[1] , edgecolor='black')
+        plt.bar(np.array(x_ticks)[np.array(x_ticks)!=None] , np.array(acc_ticks)[np.array(x_ticks)!=None], label = 'Accuracy', alpha = .7, ecolor = 'pink', width = bin_lowers[1] , edgecolor='black')
+        plt.xlabel('Confidence')
+        plt.ylabel('Accuracy')
+
+        plt.xlim(0,1)
+        plt.ylim(0,1)
+        plt.xticks(bin_boundaries,bin_boundaries.round(2))
+        plt.legend()
+        plt.savefig(os.path.join(self.ckpt_dir,f'Reliability_diagrams{plot_title}.png'))
+        plt.close('all')
+
+        plt.bar(np.array(x_ticks)[np.array(x_ticks)!=None], np.array(y_ticks_second_ticks)[np.array(x_ticks)!=None], ecolor = 'blue', width = bin_lowers[1] , edgecolor='black')
+        plt.xticks(bin_boundaries,bin_boundaries.round(2))
+        plt.yticks([0.0,0.2,0.4,0.6,0.8,1.0],[0.0,0.2,0.4,0.6,0.8,1.0])
+        plt.axvline(x=accuracies.mean() , label='Accuracy', color='red', linestyle="--")
+        plt.axvline(x=confidences.mean() , label='Avg. confidence', color='blue', linestyle="--")
+        plt.legend()
+        plt.xlabel('Confidence')
+        plt.ylabel('% of Samples')
+        plt.xlim(0,1)
+        plt.ylim(0,1)
+        plt.savefig(os.path.join(self.ckpt_dir,f'Confidence_histogram{plot_title}.png'))
+        plt.close('all')
+
+        return ece
+    
+    def seen_unseen_detection_get_ece(self, predicted_label: np.array, confidences: np.array, targets: np.array, n_bins: int=15):
+
+        bin_boundaries = np.linspace(0,1,n_bins+1)
+        bin_lowers = bin_boundaries[:-1]
+        bin_uppers = bin_boundaries[1:]
+
+        accuracies = (predicted_label == targets)
+
+        ece = 0.0
+        avg_confs_in_bins , x_ticks = [], []
+        acc_ticks, confs_ticks = [] , []
+        y_ticks_second_ticks=[]
+        for bin_lower, bin_upper in zip(bin_lowers, bin_uppers):
+            in_bin = np.logical_and(confidences > bin_lower , confidences <= bin_upper)
+            prop_in_bin = np.mean(in_bin)
+            if prop_in_bin > 0:
+                accuracy_in_bin = np.mean(accuracies[in_bin])
+                avg_confidence_in_bin = np.mean(confidences[in_bin])
+                delta = avg_confidence_in_bin - accuracy_in_bin
+
+                avg_confs_in_bins.append(delta)
+                acc_ticks.append(accuracy_in_bin)
+                confs_ticks.append(avg_confidence_in_bin)
+                x_ticks.append((bin_lower+bin_upper)/2)
+                y_ticks_second_ticks.append(prop_in_bin)
+
+                ece += np.abs(delta) * prop_in_bin
+            else:
+                avg_confs_in_bins.append(None)
+                acc_ticks.append(None)
+                confs_ticks.append(None)
+                x_ticks.append(None)
+                y_ticks_second_ticks.append(None)
+
+        plt.bar(np.array(x_ticks)[np.array(x_ticks)!=None], np.array(confs_ticks)[np.array(x_ticks)!=None], label = 'Confidence', alpha = .7, ecolor = 'blue', width = bin_lowers[1] , edgecolor='black')
+        plt.bar(np.array(x_ticks)[np.array(x_ticks)!=None] , np.array(acc_ticks)[np.array(x_ticks)!=None], label = 'Accuracy', alpha = .7, ecolor = 'pink', width = bin_lowers[1] , edgecolor='black')
+        plt.xlabel('Confidence')
+        plt.ylabel('Accuracy')
+
+        plt.xlim(0,1)
+        plt.ylim(0,1)
+        plt.xticks(bin_boundaries,bin_boundaries.round(2))
+        plt.legend()
+        plt.savefig(os.path.join(self.ckpt_dir,'SEEN-DETECTION-ECE.png'))
+        plt.close('all')
+
+        plt.bar(np.array(x_ticks)[np.array(x_ticks)!=None], np.array(y_ticks_second_ticks)[np.array(x_ticks)!=None], ecolor = 'blue', width = bin_lowers[1] , edgecolor='black')
+        plt.xticks(bin_boundaries,bin_boundaries.round(2))
+        plt.yticks([0.0,0.2,0.4,0.6,0.8,1.0],[0.0,0.2,0.4,0.6,0.8,1.0])
+        plt.axvline(x=accuracies.mean() , label='Accuracy', color='red', linestyle="--")
+        plt.axvline(x=confidences.mean() , label='Avg. confidence', color='blue', linestyle="--")
+        plt.legend()
+        plt.xlabel('Confidence')
+        plt.ylabel('% of Samples')
+        plt.xlim(0,1)
+        plt.ylim(0,1)
+        plt.savefig(os.path.join(self.ckpt_dir,'CONFIDENCE_HISTOGRAM.png'))
+        plt.close('all')
+        
         return ece
