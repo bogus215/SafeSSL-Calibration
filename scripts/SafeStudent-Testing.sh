227c4490d72db91cdb7902ef328b989903f152fd
bogus215 <wlstn215@gmail.com>
2024-03-25 16:54:09 +1300

    SafeStudent script update

--- scripts/SafeStudent-Testing.sh
+++ scripts/SafeStudent-Testing.sh
@@ -12,3 +12,17 @@ do
                                 --checkpoint-hash ${HASH[$seed]} \
                                 --for-what SafeStudent
 done
+
+SVHN_SEEDS=(1)
+HASH=(2024-03-22_02-06-26)
+
+for seed in 0
+do
+    python ./main/run_testing.py --gpus 0 --seed ${SVHN_SEEDS[$seed]} \
+                                --data svhn --server main \
+                                --n-label-per-class 50 \
+                                --mismatch-ratio 0.6 \
+                                --backbone-type wide28_2 \
+                                --checkpoint-hash ${HASH[$seed]} \
+                                --for-what SafeStudent
+done
