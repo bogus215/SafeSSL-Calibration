ca57629b827b052f558957fcbc87d89871214a50
bogus215 <wlstn215@gmail.com>
2024-03-27 13:47:51 +0900

    Ablation 4 testing update

--- tasks/testing.py
+++ tasks/testing.py
@@ -94,6 +94,8 @@ class Testing(Task):
             eval_history = self.ablation2_evaluate(open_test_loader)
         elif for_what=="Ablation3":
             eval_history = self.ablation3_evaluate(open_test_loader)
+        elif for_what=="Ablation4":
+            eval_history = self.ablation4_evaluate(open_test_loader)
         elif for_what=="IOMATCH":
             eval_history = self.iomatch_evaluate(open_test_loader)
         elif for_what=="OPENMATCH":
@@ -780,3 +782,65 @@ class Testing(Task):
         ed = logits.div(t).exp().sum(1).log().mul(t) - (logits.div(t).exp().sum(1) - logits.div(t).exp().max(1)[0] + 1e-5).log().mul(t)
 
         return ed
+    
+    @torch.no_grad()
+    def ablation4_evaluate(self, data_loader, **kwargs):
+        """Evaluation defined for a single epoch."""
+
+        steps = len(data_loader)
+        self._set_learning_phase(train=False)
+        result = {
+            'top@1': torch.zeros(1, device=self.local_rank),
+            'top@1-ova': torch.zeros(1, device=self.local_rank),
+            'ECE': np.zeros(1),
+            'ECE-ova': np.zeros(1),
+            "AUROC": np.zeros(1),
+            'In distribution over conf 0.95: ECE': np.zeros(1),
+            'In distribution under ood score 0.5: ECE': np.zeros(1),
+            "SEEN-DETECTION-ECE": np.zeros(1),
+        }
+
+        labels, logits, ova_scores, ova_logits = [], [], [], []
+
+        with Progress(transient=True, auto_refresh=False) as pg:
+
+            if self.local_rank == 0:
+                task = pg.add_task(f"[bold red] Data Testing...", total=steps)
+
+            for i, batch in enumerate(data_loader):
+
+                x = batch['x'].to(self.local_rank)
+                y = batch['y'].to(self.local_rank)
+
+                logit, features = self.backbone(x, return_feature=True)
+                probs = nn.functional.softmax(logit, 1)
+
+                ova_logit = self.backbone.scaling_logits(self.backbone.ova_classifiers(features),name='ova_cali_scaler').view(features.size(0),2,-1)
+                ova_score = ova_logit.softmax(1)[torch.arange(ova_logit.size(0)),:,probs.argmax(1)]
+                
+                labels.append(y.cpu())
+                logits.append(logit.cpu())
+                ova_scores.append(ova_score.cpu())
+                ova_logits.append(ova_logit.cpu())
+                
+                if self.local_rank == 0:
+                    desc = f"[bold pink] [{i+1}/{steps}] |"
+                    pg.update(task, advance=1., description=desc)
+                    pg.refresh()
+
+        labels, logits, ova_scores, ova_logits = torch.cat(labels,axis=0), torch.cat(logits,axis=0), torch.cat(ova_scores,axis=0), torch.cat(ova_logits,axis=0)
+        
+        in_pred = (ova_scores[:,0] < 0.5).cpu()
+        in_label = torch.where(labels<logits.size(1),1,0)
+        ood_label = torch.where(labels>=logits.size(1),1,0)
+
+        result['top@1'][0] = TopKAccuracy(k=1)(logits[labels<logits.size(1)], labels[labels<logits.size(1)])
+        result['top@1-ova'][0] = TopKAccuracy(k=1)(ova_logits.softmax(dim=1)[labels<logits.size(1),1,:], labels[labels<logits.size(1)])
+        result['ECE'][0] = self.get_ece(preds=logits[labels<logits.size(1)].softmax(dim=1).numpy(), targets = labels[labels<logits.size(1)].numpy())
+        result['ECE-ova'][0] = self.get_ece(preds=ova_logits.softmax(dim=1)[labels<logits.size(1),1,:].numpy(), targets = labels[labels<logits.size(1)].numpy(), plot_title="_all_ova")
+        result['AUROC'][0] = roc_auc_score(y_true=ood_label.cpu(), y_score=ova_scores[:,0])
+        result['In distribution over conf 0.95: ECE'][0] = self.get_ece(preds=logits[(labels<logits.size(1)) & (logits.softmax(1).max(1)[0]>=0.95)].softmax(dim=1).numpy(), targets = labels[(labels<logits.size(1)) & (logits.softmax(1).max(1)[0]>=0.95)].numpy(), plot_title="_conf_over_95")
+        result['In distribution under ood score 0.5: ECE'][0] = self.get_ece(preds=logits[(labels<logits.size(1)) & (in_pred)].softmax(dim=1).numpy(), targets = labels[(labels<logits.size(1)) & (in_pred)].numpy(), plot_title="_ood_score_under_05")
+        result['SEEN-DETECTION-ECE'][0] = self.seen_unseen_detection_get_ece(predicted_label=in_pred.numpy(),confidences=ova_scores.max(1)[0].numpy(),targets=in_label.numpy(),n_bins=15)
+        
+        return {k: v.mean().item() for k, v in result.items()}
