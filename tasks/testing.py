f3b87dafe13ffb48c912a481f57eac04f7250d54
bogus215 <wlstn215@gmail.com>
2024-04-02 12:43:52 +1300

    Testing update

--- tasks/testing.py
+++ tasks/testing.py
@@ -126,8 +126,10 @@ class Testing(Task):
             'top@1': torch.zeros(1, device=self.local_rank),
             'ECE': np.zeros(1),
             "AUROC": np.zeros(1),
+            "F1-IN": np.zeros(1),
+            "F1-OOD": np.zeros(1),
             'In distribution over conf 0.95: ECE': np.zeros(1),
-            'In distribution under ood score 0.5: ECE': np.zeros(1)
+            'In distribution under ood score 0.5: ECE': np.zeros(1),
         }
 
         labels, logits, domain_score = [], [], []
@@ -161,6 +163,7 @@ class Testing(Task):
         threshold = threshold_otsu(domain_score.cpu().numpy())
     
         in_pred = (domain_score >= threshold).cpu()
+        ood_pred = (domain_score < threshold).cpu()
         in_label = torch.where(labels<logits.size(1),1,0)
         ood_label = torch.where(labels>=logits.size(1),1,0)
 
@@ -170,6 +173,9 @@ class Testing(Task):
         result['In distribution over conf 0.95: ECE'][0] = self.get_ece(preds=logits[(labels<logits.size(1)) & (logits.softmax(1).max(1)[0]>=0.95)].softmax(dim=1).numpy(), targets = labels[(labels<logits.size(1)) & (logits.softmax(1).max(1)[0]>=0.95)].numpy(), plot_title="_conf_over_95")
         result['In distribution under ood score 0.5: ECE'][0] = self.get_ece(preds=logits[(labels<logits.size(1)) & (in_pred)].softmax(dim=1).numpy(), targets = labels[(labels<logits.size(1)) & (in_pred)].numpy(), plot_title="_ood_score_under_05")
 
+        result['F1-IN'][0] = f1_score(y_true=in_label,y_pred=in_pred)
+        result['F1-OOD'][0] = f1_score(y_true=ood_label,y_pred=ood_pred)
+        
         return {k: v.mean().item() for k, v in result.items()}
 
     @torch.no_grad()
