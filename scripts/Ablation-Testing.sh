643423af86260a0c60303d7e685d20c31c73c123
bogus215 <wlstn215@gmail.com>
2024-04-02 06:59:54 +0000

    Script Update

--- scripts/Ablation-Testing.sh
+++ scripts/Ablation-Testing.sh
@@ -116,3 +116,33 @@ do
                                 --checkpoint-hash ${HASH[$seed]} \
                                 --for-what Ablation2
 done
+
+CIFAR100_SEEDS=(1 5 10)
+HASH=(2024-03-29_13-36-05 2024-03-30_00-05-08 2024-03-30_10-09-20)
+
+for seed in 0 1 2
+do
+    python ./main/run_testing.py --gpus 0 --seed ${CIFAR100_SEEDS[$seed]} \
+                                --data cifar100 --server main \
+                                --n-label-per-class 100 \
+                                --n-valid-per-class 50 \
+                                --mismatch-ratio 0.6 \
+                                --backbone-type wide28_2 \
+                                --checkpoint-hash ${HASH[$seed]} \
+                                --for-what Ablation1
+done
+
+CIFAR100_SEEDS=(1 5 10)
+HASH=(2024-03-29_16-05-35 2024-03-30_02-34-42 2024-03-30_12-39-24)
+
+for seed in 0 1 2
+do
+    python ./main/run_testing.py --gpus 0 --seed ${CIFAR100_SEEDS[$seed]} \
+                                --data cifar100 --server main \
+                                --n-label-per-class 100 \
+                                --n-valid-per-class 50 \
+                                --mismatch-ratio 0.6 \
+                                --backbone-type wide28_2 \
+                                --checkpoint-hash ${HASH[$seed]} \
+                                --for-what Ablation2
+done
